0a4faf671e1008825e480a4ca20ddca4ce7bc3c5
Herescu Vlad <vladherescu99@gmail.com>
2022-04-17 20:51:26 +0300

    Main screen

--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartBtn : FrameLayout = findViewById(R.id.flStart)
+        flStartBtn.setOnClickListener{
+
+        }
+
     }
 }
